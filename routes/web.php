a5a2403d105ea352d6b9d4ea9f28b1235e80c688
vagrant <vagrant@homestead>
2018-10-25 08:57:18 +0000

    个人信息页面

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,8 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
 
+Route::resource('users', 'UsersController', ["only => ['show', 'update', 'edit']"]);
+// 等同于
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
