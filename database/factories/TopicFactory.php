5a82ee5edbeeb413be6279490a870a6b687e291f
vagrant <vagrant@homestead>
2018-10-26 05:33:52 +0000

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,18 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+
+    $sentence = $faker->sentence(); // 随机生成一小段文本
+
+    $updated_at = $faker->dateTimeThisMonth();
+
+    // 传参时间不得超过修改时间
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at
     ];
 });
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // 获取 faker 实例
+        $faker = app(Faker\Generator::class);
+
+        // 头像假数据
+        $avators = [
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
+            'https://iocaffcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
+        ];
+
+        $users = factory(\App\Models\User::class)->times(10)->make()
+                ->each(function ($user, $index) use($faker, $avators) {
+                // 从头像数组中随机获取一个
+                $user->avator = $faker->randomElement($avators);
+        });
+
+        // 让隐藏字段可见，并将数据集合转换为数组
+        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+
+        User::insert($user_array );
+
+        // 插入到数据库中
+        $user = User::find(1);
+        $user->name = 'mason';
+        $user->email = '9265959@qq.com';
+        $user->save();
+    }
+}
